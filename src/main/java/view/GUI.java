08cfb63909e5beb59337bdf47f3d6ec4d88817fb
Nasim Zouneibiri <111856757+nasudesu@users.noreply.github.com>
2024-02-14 10:17:28 +0200

    add base canvas

--- src/main/java/view/GUI.java
+++ src/main/java/view/GUI.java
@@ -3,10 +3,16 @@ package view;
 import controller.Controller;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 public class GUI extends Application {
+
+    private int clicks;
+
+    private double x, y;
 
     Controller Crontroller;
 
@@ -18,8 +24,43 @@ public class GUI extends Application {
     @Override
     public void start(Stage stage){
 
-        GridPane gridPane = new GridPane();
-        Scene view = new Scene(gridPane, 400, 400);
+        GridPane canvasContainer = new GridPane();
+
+        Canvas canvas = new Canvas(500, 500);
+        Canvas previewCanvas = new Canvas(500, 500);
+
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        GraphicsContext previewGc = previewCanvas.getGraphicsContext2D();
+
+
+
+        canvasContainer.setOnMouseClicked(event -> {
+            if (clicks % 2 == 0){
+                gc.moveTo(event.getX(), event.getY());
+                x = event.getX();
+                y = event.getY();
+            }
+            else {
+                previewGc.clearRect(0, 0, 500, 500);
+                gc.lineTo(event.getX(), event.getY());
+                gc.stroke();
+                gc.beginPath();
+            }
+            clicks++;
+        });
+        canvasContainer.setOnMouseMoved(event -> {
+            if (clicks % 2 == 0) return;
+            previewGc.clearRect(0, 0, 500, 500);
+            previewGc.beginPath();
+            previewGc.moveTo(x, y);
+            previewGc.lineTo(event.getX(), event.getY());
+            previewGc.stroke();
+        });
+
+        canvasContainer.add(canvas, 0, 0);
+        canvasContainer.add(previewCanvas, 0, 0);
+
+        Scene view = new Scene(canvasContainer, 500, 500);
         stage.setTitle("view.FPGUI");
         stage.setScene(view);
         stage.show();
