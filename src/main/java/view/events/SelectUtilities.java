8c2cdb9bd5538b8ad28f5372cd0d5c3f967c4a37
kassu11 <61124141+kassu11@users.noreply.github.com>
2024-03-13 18:36:35 +0200

    Fix select and add snapping

--- src/main/java/view/events/SelectUtilities.java
+++ src/main/java/view/events/SelectUtilities.java
@@ -47,18 +47,37 @@ public class SelectUtilities {
 	}
 
 	public static void moveSelectedArea(Controller controller, double x, double y) {
-		Point hoveredShape = controller.getHoveredPoint();
-		if(hoveredShape == null) {
-			for (Shape shape : controller.getShapes(Controller.SingletonType.PREVIEW)) {
-				if (shape.getType() != ShapeType.POINT) continue;
-				shape.setCoordinates(shape.getSelectedX() + x, shape.getSelectedY() + y);
-			}
-		} else {
-			for (Shape shape : controller.getShapes(Controller.SingletonType.PREVIEW)) {
-				if (shape.getType() != ShapeType.POINT) continue;
-				shape.setCoordinates(hoveredShape.getX(), hoveredShape.getY());
+		Point hoveredPoint = controller.getHoveredPoint();
+		Shape selectedShape = controller.getSelectedShape();
+
+		double fixedY = y;
+		double fixedX = x;
+		if(controller.isCtrlDown() && selectedShape.getType() == ShapeType.POINT && selectedShape.getChildren().size() == 1) {
+			Point oppositePoint = selectedShape.getChildren().get(0).getPoints().get(0);
+			if (oppositePoint == selectedShape) oppositePoint = selectedShape.getChildren().get(0).getPoints().get(1);
+
+			if(!controller.getShapes(Controller.SingletonType.PREVIEW).contains(oppositePoint)) {
+				double snappedAngle = ShapeMath.getSnapAngle(oppositePoint.getX(), oppositePoint.getY(), x, y);
+				double radius = ShapeMath.getRadius(oppositePoint.getX(), oppositePoint.getY(), x, y);
+				fixedX = ShapeMath.getSnapAngleX(oppositePoint.getX(), radius, snappedAngle) - selectedShape.getSelectedX();
+				fixedY = ShapeMath.getSnapAngleY(oppositePoint.getY(), radius, snappedAngle) - selectedShape.getSelectedY();
 			}
 		}
+
+		moveAllSelectedShapesToCursor(controller, fixedX, fixedY);
+		snapSelectedPointToHoveredPoint(selectedShape, hoveredPoint);
+	}
+
+	private static void moveAllSelectedShapesToCursor(Controller controller, double x, double y) {
+		for (Shape shape : controller.getShapes(Controller.SingletonType.PREVIEW)) {
+			if (shape.getType() != ShapeType.POINT) continue;
+			shape.setCoordinates(shape.getSelectedX() + x, shape.getSelectedY() + y);
+		}
+	}
+
+	private static void snapSelectedPointToHoveredPoint(Shape selectedShape, Point hoveredPoint) {
+		if(selectedShape == null || hoveredPoint == null) return;
+		if(selectedShape.getType() == ShapeType.POINT) selectedShape.setCoordinates(hoveredPoint.getX(), hoveredPoint.getY());
 	}
 
 	public static void finalizeSelectedShapes(Controller controller, CustomCanvas canvas, double x, double y) {
