2c7c49ee79f107a76ea530f72e7e2a134a105e16
kassu11 <61124141+kassu11@users.noreply.github.com>
2024-03-15 19:55:45 +0200

    Add point snapping drawing

--- src/main/java/view/events/DrawUtilities.java
+++ src/main/java/view/events/DrawUtilities.java
@@ -9,7 +9,20 @@ import view.types.ShapeType;
 public class DrawUtilities {
     public static void addShapesFirstPoint(Controller controller, double x, double y) {
         Point point = controller.getHoveredPoint();
-        if (point == null) point = controller.createAbsolutePoint(x, y, Controller.SingletonType.FINAL);
+        Shape hoveredShape = controller.getHoveredShape();
+        double fixedY = y;
+        double fixedX = x;
+        if (hoveredShape != null && hoveredShape.getType() == ShapeType.LINE) {
+            Point pointA = hoveredShape.getPoints().get(0);
+            double hoveredDistance = hoveredShape.calculateDistanceFromMouse(x, y);
+            double distanceFromPointA = pointA.calculateDistanceFromMouse(x, y);
+            double angle = ShapeMath.calculateAngle(pointA, hoveredShape.getPoints().get(1));
+            double radius = Math.hypot(distanceFromPointA, hoveredDistance);
+
+            fixedY = pointA.getY() + radius * Math.sin(angle);
+            fixedX = pointA.getX() + radius * Math.cos(angle);
+        }
+        if (point == null) point = controller.createAbsolutePoint(fixedX, fixedY, Controller.SingletonType.FINAL);
 
         controller.getHistoryManager().addFirstPoint(point);
 
@@ -19,13 +32,26 @@ public class DrawUtilities {
     public static Shape addShapesLastPoint(Controller controller, double x, double y, ShapeType shapeType) {
         Point startPoint = controller.getLastPoint();
         Point endPoint = controller.getHoveredPoint();
+        Shape hoveredShape = controller.getHoveredShape();
         double fixedY = y;
         double fixedX = x;
+
         if(controller.isCtrlDown()) {
             double snappedAngle = ShapeMath.getSnapAngle(startPoint.getX(), startPoint.getY(), x, y);
             double radius = ShapeMath.getRadius(startPoint.getX(), startPoint.getY(), x, y);
             fixedX = ShapeMath.getSnapAngleX(startPoint.getX(), radius, snappedAngle);
             fixedY = ShapeMath.getSnapAngleY(startPoint.getY(), radius, snappedAngle);
+        }
+
+        if (hoveredShape != null && hoveredShape.getType() == ShapeType.LINE) {
+            Point pointA = hoveredShape.getPoints().get(0);
+            double hoveredDistance = hoveredShape.calculateDistanceFromMouse(fixedX, fixedY);
+            double distanceFromPointA = pointA.calculateDistanceFromMouse(fixedX, fixedY);
+            double angle = ShapeMath.calculateAngle(pointA, hoveredShape.getPoints().get(1));
+            double radius = Math.hypot(distanceFromPointA, hoveredDistance);
+
+            fixedY = pointA.getY() + radius * Math.sin(angle);
+            fixedX = pointA.getX() + radius * Math.cos(angle);
         }
         if (endPoint == null) endPoint = controller.createAbsolutePoint(fixedX, fixedY, Controller.SingletonType.FINAL);
 
@@ -42,28 +68,30 @@ public class DrawUtilities {
         Shape hoveredShape = controller.getHoveredShape();
         double fixedY = y;
         double fixedX = x;
-        if (hoveredShape != null){
-            if (hoveredShape.getType() == ShapeType.LINE) {
-                Point pointA = hoveredShape.getPoints().get(0);
-                double hoveredDistance = hoveredShape.calculateDistanceFromMouse(x, y);
-                double distanceFromPointA = pointA.calculateDistanceFromMouse(x, y);
-                double angle = ShapeMath.calculateAngle(pointA, hoveredShape.getPoints().get(1));
-                double radius = Math.hypot(distanceFromPointA, hoveredDistance);
 
-                fixedY = pointA.getY() + radius * Math.sin(angle);
-                fixedX = pointA.getX() + radius * Math.cos(angle);
-                controller.createAbsolutePoint(fixedX, fixedY).draw(gc);
-            }
-        }
-
-        if (lastPoint == null) return;
-
-        if(controller.isCtrlDown()) {
+        if(lastPoint != null && controller.isCtrlDown()) {
             double snappedAngle = ShapeMath.getSnapAngle(lastPoint.getX(), lastPoint.getY(), x, y);
             double radius = ShapeMath.getRadius(lastPoint.getX(), lastPoint.getY(), x, y);
             fixedX = ShapeMath.getSnapAngleX(lastPoint.getX(), radius, snappedAngle);
             fixedY = ShapeMath.getSnapAngleY(lastPoint.getY(), radius, snappedAngle);
         }
+
+        if (hoveredShape != null && hoveredShape.getType() == ShapeType.LINE) {
+            Point pointA = hoveredShape.getPoints().get(0);
+            double hoveredDistance = hoveredShape.calculateDistanceFromMouse(fixedX, fixedY);
+            double distanceFromPointA = pointA.calculateDistanceFromMouse(fixedX, fixedY);
+            double angle = ShapeMath.calculateAngle(pointA, hoveredShape.getPoints().get(1));
+            double radius = Math.hypot(distanceFromPointA, hoveredDistance);
+
+            fixedY = pointA.getY() + radius * Math.sin(angle);
+            fixedX = pointA.getX() + radius * Math.cos(angle);
+            controller.createAbsolutePoint(fixedX, fixedY).draw(gc);
+        }
+
+
+        if (lastPoint == null) return;
+
+
         Point point = controller.createAbsolutePoint(fixedX, fixedY);
         if (hoveredShape != null && hoveredShape.getType() == ShapeType.POINT) point = controller.createAbsolutePoint(hoveredShape.getX(), hoveredShape.getY());
         Shape createdShape = controller.createShape(point, lastPoint.getX(), lastPoint.getY(), controller.getCurrentShape(), null);
