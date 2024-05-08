e3ffe9f33b0cce283c5b6f1363d5f966f6952eb0
Aapo Salminen <111856741+farathorr@users.noreply.github.com>
2024-05-08 16:26:23 +0300

    add sort of working door

--- src/main/java/model/shapes/Door.java
+++ src/main/java/model/shapes/Door.java
@@ -1,7 +1,5 @@
 package model.shapes;
 
-import javafx.scene.shape.Arc;
-import javafx.scene.shape.ArcType;
 import view.GUIElements.canvas.CustomCanvas;
 import view.types.ShapeType;
 
@@ -9,18 +7,29 @@ public class Door extends AbstractShape{
     public Door(Point pointA, Point pointB) {
         super(pointA, pointB);
     }
+    private int startAngle = 0;
 
     public void draw(CustomCanvas gc) {
         gc.updateCanvasColors(this);
         Point pointA = this.getPoints().get(0);
         Point pointB = this.getPoints().get(1);
-        double distanceBetweenPoints = Math.abs(pointA.getX() - pointB.getX());
+        double distanceBetweenPoints = Math.abs(Math.max(pointA.getX(), pointB.getX()) - Math.min(pointA.getX(), pointB.getX()));
+        double lengthOfAngle;
+        Point pointC = new Point(pointA.getX(), pointA.getY() - distanceBetweenPoints);
 
         gc.beginPath();
         gc.moveTo(pointA.getX(), pointA.getY());
         gc.lineTo(pointB.getX(), pointB.getY());
+        gc.moveTo(pointA.getX(), pointA.getY());
+        gc.lineTo(pointC.getX(), pointC.getY());
+        if (pointB.getX() > pointA.getX()) {
+            this.startAngle = 0;
+        } else {
+            this.startAngle = 90;
+        }
+        gc.arc(pointA.getX(), pointA.getY(), distanceBetweenPoints, distanceBetweenPoints, startAngle, 90);
+
         gc.stroke();
-        gc.strokeArc(pointA.getX(), pointA.getY() + distanceBetweenPoints, distanceBetweenPoints*2, distanceBetweenPoints , 0, 90, ArcType.OPEN);
     }
 
     @Override
