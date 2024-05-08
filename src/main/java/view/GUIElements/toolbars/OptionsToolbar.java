ec313e632d6d9b63edf22fbb08e20afdaffaa96a
Aapo Salminen <111856741+farathorr@users.noreply.github.com>
2024-05-08 22:08:44 +0300

    make filemanager into own class, refactor toolbar

--- src/main/java/view/GUIElements/toolbars/OptionsToolbar.java
+++ src/main/java/view/GUIElements/toolbars/OptionsToolbar.java
@@ -8,7 +8,6 @@ import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import model.FileManager;
 import view.GUIElements.ComboBoxItem;
 import view.GUIElements.canvas.CanvasContainer;
 import view.SettingsSingleton;
@@ -39,13 +38,10 @@ public class OptionsToolbar extends CustomToolbar {
      */
     private SettingsSingleton settings = SettingsSingleton.getInstance();
     /**
-     * File manager
-     */
-    private FileManager fileManager = FileManager.getInstance();
-    /**
      * List of text fields
      */
     private List<TextFieldWithLabel> textFields;
+    private FileManager fileManager;
     /**
      * Constructor for the options toolbar
      * @param stage stage
@@ -57,9 +53,13 @@ public class OptionsToolbar extends CustomToolbar {
         super(stage);
         this.controller = controller;
         this.canvasContainer = canvasContainer;
+        fileManager = new FileManager(canvasContainer, controller);
         this.setOrientation(Orientation.HORIZONTAL);
+
         addButton(new Button(settings.getLocalizationString("file")), "file");
         addButton(new Button(settings.getLocalizationString("settings")), "settings");
+        this.getItems().add(new Separator());
+        addButton(new Button(settings.getLocalizationString("gridSizeButton")), "gridSizeButton");
 
         TextFieldWithLabel gridWidth = new TextFieldWithLabel("gridWidth", String.format("%.0f", settings.getGridWidth()));
         TextFieldWithLabel gridHeight = new TextFieldWithLabel("gridHeight", String.format("%.0f", settings.getGridHeight()));
@@ -73,10 +73,6 @@ public class OptionsToolbar extends CustomToolbar {
             this.getItems().add(textField);
         }
 
-        this.getItems().add(new Separator());
-
-        addButton(new Button(settings.getLocalizationString("gridSizeButton")), "gridSizeButton");
-
         getButton("gridSizeButton").setOnAction(e -> {
             settings.setGridWidth(Double.parseDouble(gridWidth.getTextField().getText()));
             settings.setGridHeight(Double.parseDouble(gridHeight.getTextField().getText()));
@@ -86,57 +82,6 @@ public class OptionsToolbar extends CustomToolbar {
             System.out.println("Grid size set to " + settings.getGridWidth() + "x" + settings.getGridHeight());
         });
 
-    }
-    /**
-     * Shows the file window
-     */
-    public void showFile() {
-        Stage fileWindow = new Stage();
-
-        fileWindow.setTitle("File");
-        Button exportButton = new Button(settings.getLocalizationString("saveFloorplan"));
-        Button importButton = new Button(settings.getLocalizationString("loadFloorplan"));
-
-        JFileChooser fileChooser = new JFileChooser();
-        FileNameExtensionFilter filter = new FileNameExtensionFilter("Filter all ser files", "ser");
-        fileChooser.setDialogTitle("File Explorer");
-        fileChooser.setFileFilter(filter);
-        fileChooser.setCurrentDirectory(new File("./src/main/resources/"));
-
-        exportButton.setOnAction(e -> {
-            int response = fileChooser.showSaveDialog(null);
-            if (response == JFileChooser.APPROVE_OPTION) {
-                File selectedFile = new File(fileManager.addFileFormat(fileChooser.getSelectedFile().getAbsolutePath()));
-                fileManager.setCurrentFile(selectedFile);
-                fileManager.exportFloorPlan();
-                fileWindow.close();
-            }
-        });
-
-        importButton.setOnAction(e -> {
-            int response = fileChooser.showOpenDialog(null);
-            if (response == JFileChooser.APPROVE_OPTION) {
-                File selectedFile = new File(fileChooser.getSelectedFile().getAbsolutePath());
-                if (fileManager.checkFileFormat(selectedFile.getName())) {
-                    fileManager.setCurrentFile(selectedFile);
-                    fileManager.importFloorPlan();
-                    fileWindow.close();
-                    canvasContainer.updateAllCanvasLayers(controller);
-                }
-            }
-        });
-
-        Insets defaultInsets = new Insets(10, 10, 10, 10);
-
-        VBox fileLayout = new VBox(exportButton, importButton);
-        fileLayout.setPadding(defaultInsets);
-        fileLayout.setSpacing(10);
-        fileLayout.setAlignment(CENTER);
-
-        Scene fileScene = new Scene(fileLayout, 200, 100);
-
-        fileWindow.setScene(fileScene);
-        fileWindow.show();
     }
     /**
      * Shows the settings window
@@ -164,18 +109,6 @@ public class OptionsToolbar extends CustomToolbar {
         CheckBox showUnits = new CheckBox(settings.getLocalizationString("showUnits"));
         showUnits.setSelected(settings.isUnitsVisible());
         Button saveButton = new Button(settings.getLocalizationString("save"));
-
-        saveButton.setOnAction(e -> {
-            settings.setDrawGrid(showGrid.isSelected());
-            settings.setUnitsVisible(showUnits.isSelected());
-            settings.setLocaleWithLocaleLanguage((languageSettings.getValue().getKey()));
-            settings.setUnitsVisible(showUnits.isSelected());
-            settings.setMeasurementUnit(measurementSettings.getValue());
-            canvasContainer.updateAllCanvasLayers(controller);
-            controller.saveSettings();
-            controller.updateToolbarLocalization();
-            settingsWindow.close();
-        });
 
         List<ComboBoxItem> comboItems = Arrays.stream(settings.getAllLocalization())
                 .map(locale -> new ComboBoxItem(locale.getLocale().getLanguage(), settings.getLocalizationWithLocale(locale.getLocale()).getString("languageName"))).toList();
@@ -205,6 +138,19 @@ public class OptionsToolbar extends CustomToolbar {
 
         settingsWindow.setScene(settingsScene);
         settingsWindow.show();
+
+        saveButton.setOnAction(e -> {
+            settings.setDrawGrid(showGrid.isSelected());
+            settings.setUnitsVisible(showUnits.isSelected());
+            settings.setLocaleWithLocaleLanguage((languageSettings.getValue().getKey()));
+            settings.setUnitsVisible(showUnits.isSelected());
+            settings.setMeasurementUnit(measurementSettings.getValue());
+
+            canvasContainer.updateAllCanvasLayers(controller);
+            controller.saveSettings();
+            controller.updateToolbarLocalization();
+            settingsWindow.close();
+        });
     }
 
     /**
@@ -226,5 +172,8 @@ public class OptionsToolbar extends CustomToolbar {
             if(textField.getKey().equals("gridWidth")) textField.getTextField().setText(String.format("%.0f", settings.getGridWidth()));
             if(textField.getKey().equals("gridHeight")) textField.getTextField().setText(String.format("%.0f", settings.getGridHeight()));
         }
+    }
+    public void showFile() {
+        fileManager.showFile();
     }
 }
